04ac5fd3ac675e49c04afe0ba795f7ea698d9320
SN432 <sreynit.theara@student.cadt.edu.kh>
2024-01-28 21:53:49 +0700

    authentication

---
+++ resources/views/layouts/side-bar.blade.php
@@ -0,0 +1,12 @@
+
+<!-- resources/views/layouts/sidebar.blade.php -->
+
+<div class="bg-white text-blue-500 shadow-md h-screen w-1/5 fixed left0">
+    <!-- Sidebar content -->
+    <div class="p-4">
+        <!-- Your sidebar content goes here -->
+        <a href="#" class="block hover:bg-blue-100 py-2 px-4 rounded">Dashbaord</a>
+        <a href="#" class="block hover:bg-blue-100 py-2 px-4 rounded mt-2">Manage Package</a>
+    </div>
+</div>
+
