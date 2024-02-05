c8693792a04855e335fd67d717b862353fac0cf4
PICH PUTH SREY NEATH <pi.sreyneath@gmail.com>
2024-02-05 16:44:12 +0700

    seed admin account for backend

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,30 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Package::factory(10)->create();
-        // User::factory(5)->create()->each(function ($user){
-        //     $packageCount = rand(1, 10);
-        //     $packages = Package::inRandomOrder()->take($packageCount)->pluck("id")->toArray();
-
-        //     foreach($packages as $packageId)
-        //     {
-        //         PackageUser::factory()->create([
-        //             "user_id" => $user->id,
-        //             "package_id" => $packageId
-        //         ]);
-        //     }
-        // });
-
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        User::factory(1)->create();
-        Package::factory(10)->create()->each(function ($package) {
-            Banner::factory(1)->create(["package_id" => $package->id]);
-        });
+        $this->call([
+            UserSeeder::class,
+        ]);
     }
 }
