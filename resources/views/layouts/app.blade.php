d5588a424af12ab03f675b55bb4f4a2f2087eee6
PICH PUTH SREY NEATH <pi.sreyneath@gmail.com>
2024-03-25 03:19:25 +0700

    add close div in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    
+
 
 
     <!-- Scripts -->
@@ -54,7 +54,7 @@
                 @yield('content')
             </div>
 
-
+        </div>
     </div>
 
 </div>
